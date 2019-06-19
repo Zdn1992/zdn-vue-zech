026bf0290bc338ab6f1c369cce63497596557268
Zdn1992 <215585990@qq.com>
2019-06-19 23:24:18 +0800

    webpack-dev-server test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path')
 // 坑,vue-loader@15.*之后必须带有VueLoaderPlugin 并单独配置css-loader。
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-module.exports = {
+
+const HTMLPlugin = require('html-webpack-plugin')
+
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+const config = {
+    target:'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.js',
@@ -34,7 +41,31 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new webpack.DefinePlugin({
+            'proccess.env':{
+                NODE_ENV : isDev ? '"development"':'"production"'
+            }
+        }),
+        new VueLoaderPlugin(),
+        new HTMLPlugin()
     ]
 
 }
+
+if(isDev){
+    config.devtool = "#cheap-module-eval-source-map"
+    config.devServer = {
+        port : 8000,
+        host:"0.0.0.0",
+        overlay:{
+            errors:true
+        },
+        hot:true,
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config;
